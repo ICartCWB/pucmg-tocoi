c5f619d3e115c080ddc8e7046764b17fc7f1ea9a
Carlos Leonardo dos S. Mendes <leo@caleo.com.br>
2022-08-03 19:34:26 -0300

    Alterado o nome do token PUC.

--- src/contracts/PucCoin.sol
+++ src/contracts/PucCoin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PucCoin is Context, ERC20, Ownable {
-    constructor() public ERC20("PUCMG Coin", "PUCA") {
+    constructor() public ERC20("Popular User Coin", "PUC") {
         _mint(_msgSender(), 10000 * (10**uint256(decimals())));
     }
 }
