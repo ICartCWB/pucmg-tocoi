3017250c4cc52ed55f60ed16acfdb99608a73a07
CARLOS LEONARDO DOS S. MENDES <75801821+caleoleo@users.noreply.github.com>
2022-10-23 11:46:48 -0300

    Update PucMarketplace.sol

--- src/contracts/PucMarketplace.sol
+++ src/contracts/PucMarketplace.sol
@@ -1,83 +1,86 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "./PucCoin.sol";
 
 contract PucMarketplace {
-  string public name;
-  PucCoin private _token;
-  uint public productCount=0;
-  mapping(uint => Product) public products;
+    string public name;
+    PucCoin private _token;
+    uint public productCount=0;
+    mapping(uint => Product) public products;
 
-struct Product {
-  uint id;
-  string name;
-  uint price;
-  address payable owner;
-  bool purchased;
-}
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
 
-event ProductCreated (
-  uint id,
-  string name,
-  uint price,
-  address payable owner,
-  bool purchased
-);
+    event ProductCreated (
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
 
-event ProductPurchased (
-  uint id,
-  string name,
-  uint price,
-  address payable owner,
-  bool purchased
-);
+    event ProductPurchased (
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
 
-  constructor(PucCoin token) public {
-    name = "PUCMG Marketplace";
-    _token = token;
-  }
+    constructor(PucCoin token) {
+        name = "PUCMG Marketplace";
+        _token = token;
+    }
 
-  function createProduct(string memory _name, uint _price) public {
-    //Require a name
-    require(bytes(_name).length > 0, "Enter a valid name");
-    //Requiere a valid price
-    require(_price > 0, "Enter a valid price");
-    //Increment product count
-    productCount++;
-    //Create the product
-    products[productCount] = Product(productCount, _name, _price, msg.sender, false);
-    //Trigger an event
-    emit ProductCreated(productCount, _name, _price, msg.sender, false);
-  }
+    function createProduct(string memory _name, uint _price) public {
+        //Require a name        
+        require(bytes(_name).length > 0, "Enter a valid name");
+        //Requiere a valid price
+        require(_price > 0, "Enter a valid price");
+        //Increment product count
+        productCount++;
+        //Create the product
+        products[productCount] = Product(productCount, _name, _price, payable(msg.sender), false);
+        //Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
 
-  function purchaseProduct(uint _id) public {
-    //Fetch the product and make a copy of it
-    Product memory _product = products[_id];
-    //Fetch the owner
-    address payable _seller = _product.owner;
-    //Make sure the product has valid id
-    require(_product.id > 0 && _product.id <= productCount, "Enter valid id");
+    function purchaseProduct(uint _id) public {
+        //Fetch the product and make a copy of it
+        Product memory _product = products[_id];
+        //Fetch the owner
+        address payable _seller = payable(_product.owner);
+        //Make sure the product has valid id
+        require(_product.id > 0 && _product.id <= productCount, "Enter valid id");
 
-    //Require that there is enough PUCAs to confirm the transaction
-    uint balance = _token.balanceOf(msg.sender);
-    require(balance >= _product.price, "Transfer the correct amount");
+        address buyer = msg.sender;
 
-    //Require that the product has not been purchased already
-    require(!_product.purchased, "Product has been purchased");
-    //Require that the buyer is not the seller
-    require(msg.sender != _seller, "Buyer cannot be seller");
+        //Require that there is enough PUCAs to confirm the transaction
+        uint balance = _token.balanceOf(buyer);
+        require(balance >= _product.price, "Transfer the correct amount");
 
-    //Transfer ownership to the buyer
-    _product.owner = msg.sender;
-    //Mark as purchased
-    _product.purchased = true;
-    //Update the product
-    products[_id] = _product;
+        //Require that the product has not been purchased already
+        require(!_product.purchased, "Product has been purchased");
+        //Require that the buyer is not the seller
+        require(buyer != _seller, "Buyer cannot be seller");
 
-    //Pay the seller by sending them PUCs
-    _token.transferFrom(buyer, _seller, _product.price);
+        //Transfer ownership to the buyer
+        _product.owner = buyer;
+        //Mark as purchased
+        _product.purchased = true;
+        //Update the product
+        products[_id] = _product;
 
-    //Trigger an event
-    emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
-  }
+        //Pay the seller by sending them PUCs
+        _token.transferFrom(buyer, _seller, _product.price);
+
+        //Trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, buyer, true);
+    }
 }
